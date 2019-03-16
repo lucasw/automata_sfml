109a2da6f87ba4ee291291fb9db5a81510e08529
Lucas Walter <wsacul@gmail.com>
2019-03-16 13:27:33 -0700

    Draw map on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,38 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
+class Person
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+};
+
+class Land
+{
+public:
+  Land(const std::string& path)
+  {
+    if (!map_tex_.loadFromFile(path + "map.png")) {
+      throw std::runtime_error("couldn't load image from disk " + path);
+    }
+    sprite_.setTexture(map_tex_);
+
+    const auto width = map_tex_.getSize().x;
+    const auto height = map_tex_.getSize().y;
+
+    std::cout << width << " " << height << "\n";
+  }
+
+  sf::Texture map_tex_;
+  sf::Sprite sprite_;
+};
+
+int main(int argn, char* argv[])
+{
+  sf::RenderWindow window(sf::VideoMode(1000, 800), "automata");
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
+
+  Land land("../automata_sfml/data/");
 
   while (window.isOpen()) {
     sf::Event event;
@@ -13,7 +41,8 @@ int main()
         window.close();
     }
     window.clear();
-    window.draw(shape);
+    // window.draw(shape);
+    window.draw(land.sprite_);
     window.display();
   }
 
